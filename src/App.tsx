ba3e174fb4ef4bb0409185ab4898636ac6b68e5c
Maor Cohen <cohenm818@gmail.com>
2024-12-14 16:03:09 +0200

    console log supabase key

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ const App = () => {
 
 	const supabaseUrl = "https://yplzzgbwrphcwkrpdpjh.supabase.co";
 	const supabaseKey = process.env.SUPABASE_KEY;
+	console.log(process.env.SUPABASE_KEY);
 	const supabase = createClient(supabaseUrl, supabaseKey as string);
 
 	const addUser = async () => {
